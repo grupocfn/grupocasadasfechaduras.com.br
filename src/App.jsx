831e3b4e5bded1478824532edc889aa3cfbcbb28
patrick2m <pmm12@hotmail.com>
2023-03-21 12:10:10 -0300

    correção de página para gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />}/>
+        <Route path='/grupocasadasfechaduras' element={<Home />}/>
         <Route path='/avaliacaocfn' element={<AvaliacaoCFN />}/>
       </Routes>
     </BrowserRouter>
