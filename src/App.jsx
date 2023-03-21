ff7f4914b1cf4a9d435da5aff1267ccc61427b84
patrick2m <pmm12@hotmail.com>
2023-03-21 13:04:42 -0300

    teste

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,11 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/grupocasadasfechaduras' element={<Home />}/>
-        <Route path='/grupocasadasfechaduras/avaliacaocfn' element={<AvaliacaoCFN />}/>
-        <Route path='/grupocasadasfechaduras/avaliacaocfer' element={<AvaliacaoCFER />}/>
-        <Route path='/grupocasadasfechaduras/avaliacaocfsg' element={<AvaliacaoCFSG />}/>
-        <Route path='/grupocasadasfechaduras/avaliacaobb' element={<AvaliacaoBB />}/>
+        <Route path='/' element={<Home />}/>
+        <Route path='/avaliacaocfn' element={<AvaliacaoCFN />}/>
+        <Route path='/avaliacaocfer' element={<AvaliacaoCFER />}/>
+        <Route path='/avaliacaocfsg' element={<AvaliacaoCFSG />}/>
+        <Route path='/avaliacaobb' element={<AvaliacaoBB />}/>
       </Routes>
     </BrowserRouter>
   );
