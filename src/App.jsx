5b5825fc6bebf36a7b4d6f628e0ecbe1bc379e95
patrick2m <pmm12@hotmail.com>
2023-03-21 12:35:49 -0300

    teste botando rota da avaliacaocfn para /grupocfn/avaliacaocfn

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { Route } from 'react-router';
 import { BrowserRouter, Routes } from 'react-router-dom';
 import './App.scss';
+import AvaliacaoBB from './pages/AvaliacaoBB';
+import AvaliacaoCFER from './pages/AvaliacaoCFER';
 import AvaliacaoCFN from './pages/AvaliacaoCFN';
+import AvaliacaoCFSG from './pages/AvaliacaoCFSG';
 import { Home } from './pages/Home';
 
 
@@ -9,8 +12,11 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/grupocasadasfechaduras' element={<Home />}/>
-        <Route path='/avaliacaocfn' element={<AvaliacaoCFN />}/>
+        <Route path='/grupocfn' element={<Home />}/>
+        <Route path='/grupocfn/avaliacaocfn' element={<AvaliacaoCFN />}/>
+        <Route path='/avaliacaocfer' element={<AvaliacaoCFER />}/>
+        <Route path='/avaliacaocfsg' element={<AvaliacaoCFSG />}/>
+        <Route path='/avaliacaobb' element={<AvaliacaoBB />}/>
       </Routes>
     </BrowserRouter>
   );
